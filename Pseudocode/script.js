b426e6e3732be1cd45cdf001442db5aaa7e6cd90
Visa <visalakshi.cse@gmail.com>
2020-03-12 10:06:49 -0500

    Script file added

---
+++ Pseudocode/script.js
@@ -0,0 +1,6 @@
+1.One Div container is created to hold all the symbols
+2.Another Div container is created to hold the Div container already created
+3.Only If the credit is added the start button is enabled
+4.Once start button is clicked symbols are randomly placed
+5.Through queryselector method ifthree same symbols lie horizontally or vertically credit is awarded
+6.until the credit is zero,user can click the start button
